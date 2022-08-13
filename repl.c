310ad2ae46592259bd43a91c53472c39c885a869
Maxwell D. Dorliea <deboy170@gmail.com>
2022-08-13 00:34:16 +0000

    Fixed Bug in task 8

--- repl.c
+++ repl.c
@@ -3,6 +3,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * exit_arg - handle exit with argument
+ * @cmd: array of command enter
+ * @argv: array of argument
+ * @buf: command enter
+ * Return: int
+ */
+
+int exit_arg(char **cmd, char **argv, char *buf)
+{
+	int status = _atoi(cmd[1]);
+
+	if (status < 0 || (status == 0 && cmd[1][0] != '0'))
+	{
+		write(2, argv[0], _strlen(argv[0]));
+		write(2, ": 1: exit: Illegal number: ", 27);
+		write(2, cmd[1], _strlen(cmd[1]));
+		write(2, "\n", 1);
+		free(buf);
+		exit(2);
+	}
+
+	free(buf);
+	exit(status);
+}
 
 /**
  * read_cmd - reads the commands from the terminal till newline char
@@ -42,12 +67,10 @@ int REPL(char *buf, size_t buflen, char **cmd, char **argv, char **env)
 		if (isatty(STDIN_FILENO))
 			print_prompt();
 		read_cmd(&buf, &buflen);
-
 		if (buf[0] == '\0' || _strcmp(buf, "\n") == 0)
 		{
 			continue;
 		}
-
 		if (_strcmp(buf, "exit\n") == 0)
 		{
 			free(buf);
@@ -61,13 +84,9 @@ int REPL(char *buf, size_t buflen, char **cmd, char **argv, char **env)
 		parser(buf, cmd);
 		if (cmd[0] == NULL)
 			continue;
-
 		if (_strcmp(cmd[0], "exit") == 0 && get_arr_len(cmd) == 2)
 		{
-			int status = _atoi(cmd[1]);
-
-			free(buf);
-			exit(status);
+			exit_arg(cmd, argv, buf);
 		}
 		execute_cmd(cmd, argv, env);
 	} while (1);
